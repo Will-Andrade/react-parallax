85a79813fbcd92f0bc8287bc3817d498b67eb907
Will-Andrade <willian.santos.andra@outlook.com>
2023-02-18 00:59:24 -0400

    creates parallax scrolling with react spring

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from 'react';
+import moon from './moon.png';
+import land from './land.png';
+import cat from './cat.gif';
+
+// The layer is a page that takes up the entire screen
+import { IParallax, Parallax, ParallaxLayer } from '@react-spring/parallax';
 
 function App() {
+  const ref = useRef<IParallax | null>(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Parallax pages={4} ref={ref}>
+      <ParallaxLayer
+        offset={0} 
+        speed={1}
+        factor={2}
+        style={{
+          backgroundImage: `url(${moon})`,
+          backgroundSize: 'cover'
+        }}
+      />
+
+      <ParallaxLayer 
+        offset={2} 
+        speed={1}
+        factor={4}
+        style={{
+          backgroundImage: `url(${land})`,
+          backgroundSize: 'cover'
+        }}
+      />
+
+      <ParallaxLayer
+        sticky={{ start: 0.9, end: 2.5 }}
+        style={{ textAlign: 'center' }}
+      >
+        <img src={cat} alt="kitty parachute" />
+      </ParallaxLayer>
+
+      <ParallaxLayer
+        offset={0.2}
+        speed={0.05}
+        onClick={() => ref.current?.scrollTo(3)}
+      >
+        <h2>Welcome to my parallax!</h2>
+      </ParallaxLayer>
+      <ParallaxLayer
+        offset={3.2}
+        speed={2}
+        onClick={() => ref.current?.scrollTo(0)}
+      >
+        <h2>Hello, World.</h2>
+      </ParallaxLayer>
+    </Parallax>
   );
 }
 
